a8eab7d14a314642e9c87fe31113ba2e6e75b329
Isamovic <menchov@orange.fr>
2023-08-18 16:21:58 +0200

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,66 @@
 "use strict";
 
+const body = document.querySelector("body");
+const button1 = document.createElement("button");
+const button2 = document.createElement("button");
+const button3 = document.createElement("button");
+const resultat = document.createElement("div");
+const joueur1 = document.querySelector(".j1");
+const joueur2 = document.querySelector(".j2");
+
+const score = document.querySelector(".score");
+
+resultat.classList.add("result");
+resultat.textContent = "En attente du résultat !";
+
+button1.textContent = "Rock";
+button1.setAttribute("id", "rock");
+
+button2.textContent = "Paper";
+button2.setAttribute("id", "paper");
+
+button3.textContent = "Scisssors";
+button3.setAttribute("id", "scissors");
+
+body.appendChild(button1);
+body.appendChild(button2);
+body.appendChild(button3);
+body.appendChild(resultat);
+
+/* état du jeu */
+
+let game = true;
+
+const btn = document.querySelectorAll("button");
+
+btn.forEach(function (nb) {
+  nb.addEventListener("click", function () {
+    if (game === true) {
+      play(nb.getAttribute("id"), getComputerChoice());
+      displayScore();
+      if (scorePlayer === 5 || scorePlayer2 === 5) {
+        game = false;
+        resultat.textContent =
+          scorePlayer > scorePlayer2
+            ? "Le joueur gagne "
+            : "L'ordinateur gagne";
+      }
+    }
+  });
+});
+
+/* Suivi du score */
+
+let scorePlayer = 0;
+let scorePlayer2 = 0;
+
+/* functions */
+
+function displayScore() {
+  joueur1.textContent = scorePlayer;
+  joueur2.textContent = scorePlayer2;
+}
+
 function getComputerChoice() {
   const choix = ["rock", "paper", "scissors"];
   const result = Math.trunc(Math.random() * 3);
@@ -9,41 +70,38 @@ function getComputerChoice() {
 function play(playerSelection, computerSelection) {
   if (playerSelection === "rock") {
     if (computerSelection === "scissors") {
-      console.log(`Vous gagnez! ${playerSelection} bats ${computerSelection}`);
+      scorePlayer++;
+      resultat.textContent = `Vous gagnez! ${playerSelection} bats ${computerSelection}`;
     } else if (computerSelection === playerSelection) {
-      console.log("Egalite");
+      resultat.textContent = "Egalite";
     } else {
-      console.log(`Vous perdez! ${computerSelection} bats ${playerSelection} `);
+      scorePlayer2++;
+      resultat.textContent = `Vous perdez! ${computerSelection} bats ${playerSelection} `;
     }
   }
 
   if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      console.log(`Vous gagnez! ${playerSelection} bats ${computerSelection}`);
+      scorePlayer++;
+
+      resultat.textContent = `Vous gagnez! ${playerSelection} bats ${computerSelection}`;
     } else if (computerSelection === playerSelection) {
-      console.log("Egalite");
+      resultat.textContent = "Egalite";
     } else {
-      console.log(`Vous perdez! ${computerSelection} bats ${playerSelection} `);
+      scorePlayer2++;
+
+      resultat.textContent = `Vous perdez! ${computerSelection} bats ${playerSelection} `;
     }
   }
   if (playerSelection === "scissors") {
     if (computerSelection === "paper") {
-      console.log(`Vous gagnez! ${playerSelection} bats ${computerSelection}`);
+      scorePlayer++;
+      resultat.textContent = `Vous gagnez! ${playerSelection} bats ${computerSelection}`;
     } else if (computerSelection === playerSelection) {
-      console.log("Egalite");
+      resultat.textContent = "Egalite";
     } else {
-      console.log(`Vous perdez! ${computerSelection} bats ${playerSelection} `);
+      scorePlayer2++;
+      resultat.textContent = `Vous perdez! ${computerSelection} bats ${playerSelection} `;
     }
   }
 }
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt(
-      "Veuillez faire un choix entre paper, scissors et rock"
-    ).toLowerCase();
-    play(playerSelection, getComputerChoice());
-  }
-}
-
-game();
